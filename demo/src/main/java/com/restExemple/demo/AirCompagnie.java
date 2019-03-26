4bc4b17c653f51ec32cb91b8950425d0e1009e21
ikhanfir <ikhanfir@DebianDevGnome.local>
2019-03-26 11:43:07 +0100

    commit test

--- demo/src/main/java/com/restExemple/demo/AirCompagnie.java
+++ demo/src/main/java/com/restExemple/demo/AirCompagnie.java
@@ -8,8 +8,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
